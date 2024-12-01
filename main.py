a089b2eeea8097247c26e89bf833f409d89473e4
uprcs-voyager <greenglass0k@gmail.com>
2024-12-01 22:49:29 +0800

    auto_update ui sama update tampilan page suhu

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow
 from PyQt5 import QtWidgets, uic
 from updated_calc import Ui_Dialog
 
@@ -9,21 +9,20 @@ class MainDialog(QDialog):
 
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Dialog()  # Inisialisasi UI
-        self.ui.setupUi(self)  # Menyiapkan UI
+        uic.loadUi("updated_calc.ui", self)
 
 
-        self.ui.submitpilihan.clicked.connect(self.navigate_to_page) # Set halaman pertama adalah page_welcome
-        self.ui.tombol_home.clicked.connect(self.go_to_home)
+        self.submitpilihan.clicked.connect(self.navigate_to_page) # Set halaman pertama adalah page_welcome
+        self.tombol_home.clicked.connect(self.go_to_home)
 
     def navigate_to_page (self) :
-        selected_option = self.ui.Opsimode.currentText()
+        selected_option = self.Opsimode.currentText()
 
         if selected_option == "Konversi Suhu" :
-            self.ui.stackedWidget.setCurrentIndex(1)
+            self.stackedWidget.setCurrentIndex(1)
 
     def go_to_home (self):
-        self.ui.stackedWidget.setCurrentIndex(0)
+        self.stackedWidget.setCurrentIndex(0)
 
 # aritmatika page_suhu
     def konversi_suhu (self, suhu, selected_konversi):
@@ -32,12 +31,24 @@ class MainDialog(QDialog):
 
 # tombol page_suhu
     def tombol_angka_klik(self, angka):
-        current_text = self.ui.input_kanan.text()
+        current_text = self.input_kanan.text()
         new_text = current_text + str(angka)
-        self.ui.input_kanan.setText(new_text)
+        self.input_kanan.setText(new_text)
+
+
+
+
+
+
+
+
+class MainWindow(QMainWindow) :
+    def __init__(self):
+        super().__init__()
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication([])
     main_dialog = MainDialog()
     main_dialog.show()
     sys.exit(app.exec_())
